eded42254f46350f8413d264382d8bffcb66bab8
James Coleman <jcolem22@msudenver.edu>
2020-05-17 12:53:42 -0600

    Massive refactor of emplyees.py.

--- program/employees.py
+++ program/employees.py
@@ -1,54 +1,75 @@
-from program.hr import (
-    SalaryPolicy,
-    HourlyPolicy,
-    CommissionPolicy
-)
-from program.productivity import (
-    ManagerRole,
-    SecretaryRole,
-    SalesRole,
-    FactoryRole,
-)
+from hr import PayrollSystem
+from contacts import AddressBook
+from productivity import ProductivitySystem
+
+class EmployeeDatabase:
+""" 
+manager = employees.Manager(1, 'Michae Scott', 2400)
+secretary = employees.Secretary(1, 'Pam Beasly', 1200)
+sales_person = employees.SalesPerson(3, 'Andy Bernard', 600, 545)
+factory_worker = employees.FactoryWorker(2, 'Roy Anderson', 35, 23.40)
+temporary_secretary = employees.TemporarySecretary(5, 'Ryan', 30, 22)
+"""
+
+
+    def __init__(self):
+        self._employees = [
+            {
+                'id': 1,
+                'name': 'Michae Scott',
+                'role': 'manager',
+            },
+            {
+                'id': 2,
+                'name': 'Pam Beasly',
+                'role': 'secretary',
+            },
+            {
+                'id': 3,
+                'name': 'Andy Bernard',
+                'role': 'sales',
+            },
+            {
+                'id': 4
+                'name': 'Roy Anderson',
+                'role': 'factory',
+            },
+            {
+                'id': 5,
+                'name': 'Ryan'
+                'role': 'secretary'
+            },
+        ]
+        self.productivity = ProductivitySystem
+        self.employee_address = AddressBook
+        self.payroll = PayrollSystem
+
+    def employees(self):
+        return [self._create_employee(**data) for data in self._employees]
+     
+    def _create_employee(self, id, name, role):
+        address = self.employee_address.get_employee_address(id)
+        employee_roll = self.productivity.get_roll(role)
+        payroll_policy = self.payroll.get_policy(id)
+        return Employee(id, name, address, employee_roll, payroll_policy)
 
 
 class Employee():
 
-    def __init__(self, id, name):
+    def __init__(self, id, name, address, role, payroll):
         self.id = id
         self.name = name
-        self.address = None
+        self.address = address
+        self.payroll = payroll
+        self.employee_roll = role
 
+    def work(self, hours):
+        tasks = self.role.work(hours)
+        print(f'Employee {self.id} - {self.name}:')
+        print(f'- {tasks}')
+        print('')
+        self.payroll.track_work(hours)
 
-class Manager(Employee, ManagerRole, SalaryPolicy):
+    def calculate_payroll(self):
+        return self.payroll.calculate_payroll() 
 
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
-
-
-class Secretary(Employee, SecretaryRole, SalaryPolicy):
-
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
-
-
-class SalesPerson(Employee, SalesRole, CommissionPolicy):
-
-    def __init__(self, id, name, weekly_salary, commission):
-        CommissionPolicy.__init__(self, weekly_salary, commission)
-        super().__init__(id, name)
-
-
-class FactoryWorker(Employee, FactoryRole, HourlyPolicy):
-
-    def __init__(self, id, name, hours, hourly_rate):
-        HourlyPolicy.__init__(self, hours, hourly_rate)
-        super().__init__(id, name)
-
-
-class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
-
-    def __init__(self, id, name, hours, hourly_rate):
-        HourlyPolicy.__init__(self, hours, hourly_rate)
-        super().__init__(id, name)
