77b0762509174758d812e598eb828f84f650996c
James Coleman <jcolem22@msudenver.edu>
2020-05-17 11:49:50 -0600

    implemented address in hr and tested it.

--- program/main.py
+++ program/main.py
@@ -1,4 +1,5 @@
 import hr
+import contacts
 import employees
 import productivity
 
@@ -8,6 +9,19 @@ secretary = employees.Secretary(1, 'Pam Beasly', 1200)
 sales_person = employees.SalesPerson(3, 'Andy Bernard', 600, 545)
 factory_worker = employees.FactoryWorker(2, 'Roy Anderson', 35, 23.40)
 temporary_secretary = employees.TemporarySecretary(5, 'Ryan', 30, 22)
+
+manager.address = contacts.Address(
+	'5234 Tuneful st.',
+	'Scranton',
+	'PA',
+	'20983'
+)
+secretary.address = contacts.Address(
+	'7834 Langly ln.',
+	'Scranton',
+	'PA',
+	'20897'
+)
 
 staff = [manager, secretary, sales_person, factory_worker, temporary_secretary]
 
--- test/test_hr.py
+++ test/test_hr.py
@@ -1,5 +1,5 @@
 import unittest
-from program import hr, employees
+from program import hr, employees, contacts
 
 
 class TestHr(unittest.TestCase):
@@ -17,6 +17,13 @@ class TestHr(unittest.TestCase):
         sales_person = employees.SalesPerson(3, 'Andy Bernard', 600, 545)
         factory_worker = employees.FactoryWorker(2, 'Roy Anderson', 35, 23.40)
         temporary_secretary = employees.TemporarySecretary(5, 'Ryan', 30, 22)
+
+        manager.address = contacts.Address(
+            '5234 Tuneful st.',
+            'Scranton',
+            'PA',
+            '20983'
+        )
 
         staff = [manager, secretary, sales_person, factory_worker, temporary_secretary]
 
